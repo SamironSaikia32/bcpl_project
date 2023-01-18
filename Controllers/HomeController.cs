867151fe55f2986f57c678b298285753235b1239
ACER <ACER@DESKTOP-A5664D2>
2023-01-18 15:53:16 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using duit_net_mvc.Models;
+using duit_net_mvc.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace duit_net_mvc.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly DatabaseContext db;  // Database Context
+        private readonly IWebHostEnvironment webHostEnvironment;  // to get server absolute paths
+        public HomeController(DatabaseContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            db = context;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CurrentOpenings()
+        {
+            List<Advertisement> advertisements = db.Advertisement.ToList();
+            return View(advertisements);
+        }
+
+        public IActionResult UserList()
+        {
+            List<User> users = db.User.ToList();
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Apply(int id)
+        {
+            Advertisement advertisement = db.Advertisement.Find(id);
+            if(advertisement != null)
+            {
+                ViewBag.advt_id = id;
+                ViewBag.advt_title = advertisement.Title;
+                TempData["data"] = "data";
+
+                ApplicationViewModel newVM = new()
+                {
+                    AdvertisementId = advertisement.AdvertisementId
+                };
+                return View(newVM);
+            }
+            else
+            {
+                TempData["error_msg"] = "Invalid Advertisement";
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> Apply(int advertisementID, string userName, List<IFormFile> fileUploads)
+        {
+            Advertisement advertisement = db.Advertisement.Find(advertisementID);
+
+            if (advertisement != null)
+            {
+                Application newApp = new()
+                {
+                    UserName = userName,
+                };
+
+                newApp.User = "user"; // info from current logged in user
+
+                string contentRootPath = webHostEnvironment.ContentRootPath; // will return path for the Content folder
+                string[] paths = { contentRootPath, "AppAttachments", advertisement.AdvertisementId.ToString() };
+                string fileSaveDir = Path.Combine(paths);
+                Directory.CreateDirectory(fileSaveDir);
+
+
+                List<ApplicationAttachment> attachments = new();
+                foreach (var item in fileUploads)
+                {
+                    if (item.ContentType.Contains("image/") || item.ContentType == "application/pdf")
+                    {
+                        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(item.FileName);
+                        string fileSavePath = Path.Combine(fileSaveDir, fileName);
+
+                        ApplicationAttachment attachment = new()
+                        {
+                            FileName = fileName,
+                            FileType = item.ContentType,
+                            FilePath = fileSavePath
+                        };
+                        attachments.Add(attachment);
+
+                        // saving large file as stream
+                        using var stream = System.IO.File.Create(fileSavePath);
+                        await item.CopyToAsync(stream);
+                    }
+                }
+
+                newApp.ApplicationAttachment = attachments;
+
+                if (advertisement.Application != null && advertisement.Application.Any())
+                {
+                    advertisement.Application.Add(newApp);
+                }
+                else
+                {
+                    List<Application> applications = new();
+
+                    applications.Add(newApp);
+
+                    advertisement.Application = applications;
+                }
+
+                db.Entry(advertisement).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserAccountController.cs
@@ -0,0 +1,132 @@
+﻿using duit_net_mvc.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace duit_net_mvc.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        private readonly DatabaseContext db;
+        public UserAccountController(DatabaseContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(string name, string email, string password, string contact)
+        {
+            User newUser = new User(); // creating object of class User with name newUser
+
+            newUser.Email = email;  // calling setter function of parameter Email
+            newUser.Password = HashPassword(password); ;
+            newUser.Name = name;
+            newUser.ContactNumber = contact;
+
+            db.User.Add(newUser);
+            db.SaveChanges();
+
+            TempData["succ_msg"] = "Account has been successfully registerd. Please Login to continue";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+
+            User user = db.User.Where(m => m.Email == email).First();
+            if (user != null)
+            {
+                if (ValidatePassword(password, user.Password))
+                {
+                    // login successful
+                    var claims = new List<Claim>
+                    {
+                        new Claim("userid", user.UserId.ToString()),
+                        new Claim(ClaimTypes.Name, user.Name),
+                        new Claim(ClaimTypes.Email, user.Email),
+                        new Claim(ClaimTypes.MobilePhone, user.ContactNumber),
+                    };
+
+
+                    if (user.Name == "AdminUser")
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                    }
+
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var authProperties = new AuthenticationProperties
+                    {
+                        AllowRefresh = true,
+                        IssuedUtc = DateTimeOffset.UtcNow,
+                    };
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity),
+                        authProperties);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // login unsuccessful....gvh
+                    return RedirectToAction("Account", "Index");
+                }
+            }
+            else
+            {
+                // login unsuccessful....gvh
+                return RedirectToAction("Account", "Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            // HttpContext.Session.Clear();
+            // Clear the existing external cookie
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index");
+        }
+
+        private static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(12);
+        }
+        public static string HashPassword(string password)
+        {
+            // write manual codes or use existing security library
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, correctHash);
+        }
+    }
+}
